1a1b23ad36b6d4465f6f5b54cc10c7a28fda3dda
devwaseem <waseem07799@gmail.com>
2017-12-10 23:16:02 +0530

    minor code changes

--- FacePass Webservice/fc.py
+++ FacePass Webservice/fc.py
@@ -1,49 +1,46 @@
 import numpy as np
 import cv2
 import os
-from PIL import Image,ImageFile
+from PIL import Image
 import cv2.face as fc
 import time
 import zipfile,os
-import threading
+
 
 #--------------------------------------------------------#
 #    create dataset from image or batch of images
 #--------------------------------------------------------#
 
-def detectFace(uid,path,sn):
-    if not os.path.exists('dataset'):
-        os.makedirs('dataset')
+def detectFace(uid,path):
     if path.endswith('.jpg'):
-        sampleNum=getSampleNum(uid)
-        faces,gray= getFaces(path)
+        req_size = 400,400
+        _tempImg = Image.open(path)
+        _tempImgSize = _tempImg.size
+        if _tempImgSize == req_size :
+            _tempImg.thumbnail(req_size, Image.ANTIALIAS)
+            _tempImg.save(path,optimize=True)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        sampleNum=0
+        color = Image.open(path)
+        color = np.array(color)
+        img = Image.open(path).convert('L')
+        gray = np.array(img,'uint8')
+        faces = face_cascade.detectMultiScale(gray)
         if(len(faces)>0):
             for (x,y,w,h) in faces:
+                sampleNum=sampleNum+1
                 cv2.imwrite("dataset/User."+str(uid)+'.'+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
-            return len(faces)
+#                cv2.rectangle(color,(x,y),(x+w,y+h),(255,0,0),2)
+        return len(faces)
 
 def dataSetCreator(uid,path,isDir):
+
         if(isDir):
-            face = 0
             imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
             for path in imagePaths:
-                a = detectFace(uid,path,face)
-                if type(a) is int:
-                    face += a
-                else:
-                    print "something error occured"
-                print face
-            return face
+                return detectFace(uid,path)
         else:
-           return detectFace(uid,path,0)
-
-def getSampleNum(Id):
-    imagePaths = [os.path.join('dataset',f) for f in os.listdir('dataset')]
-    print('images {}'.format(imagePaths))
-    paths = list(filter( lambda x: x.startswith('dataset/User.'+str(Id)), imagePaths))
-    print('filtered {}'.format(paths))
-    return len(paths)
-
+           return detectFace(uid,path)
 
 #--------------------------------------------------------#
 #    create dataset with webcam
@@ -71,25 +68,20 @@ def dataSetCreatorWithCamera(val):
     cv2.destroyAllWindows()
 
 
+
+
 def createDatasetFromZip(Id,path):
-    if not os.path.exists('dataset'):
-        os.makedirs('dataset')
     try:
         zip_ref = zipfile.ZipFile(path, 'r')
         model_path = 'model/%d/'%Id
         zip_ref.extractall(model_path)
         zip_ref.close()
-        print "zip created"
         faces = dataSetCreator(Id,model_path,isDir=True)
-        print "faces %d"%faces
-        if faces > 2 :
+        if faces > 5 :
             return (faces,"Your face has been has been stored in our database")
         else:
             return (faces,"please capture the photo in good lightning condition!")
-    except Exception as ex:
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print message
+    except :
         return (0,"Please provide a zip file with your faces")
 
 
@@ -101,8 +93,11 @@ def createDatasetFromZip(Id,path):
 def train():
     recognizer = fc.LBPHFaceRecognizer_create()
     detector= cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
-    path='dataset'
+    path='datasets'
+
     def getImageWithID(path):
+#        imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+#        imagePaths = [if f.endswith('.jpg'): f for f in imagePaths]
         imagePaths = []
         for i in [os.path.join(path,f) for f in os.listdir(path)]:
             if i.endswith('.jpg'):
@@ -111,10 +106,10 @@ def train():
         faceSamples=[]
         Ids=[]
         for imagePath in imagePaths:
-            if imagePath == '.DS_Store':  #removing unneccesary files if created from macos
-                os.remove('.DS_Store')
+            if path == '.DS_Store':
+                os.remove(path)
                 continue
-            pilImage=Image.open(imagePath)
+            pilImage=Image.open(imagePath).convert('L')
             imageNp=np.array(pilImage,'uint8')
             Id=int(os.path.split(imagePath)[-1].split(".")[1])
             faces=detector.detectMultiScale(imageNp)
@@ -138,16 +133,26 @@ def train():
 def detecter(path):
     recognizer = fc.LBPHFaceRecognizer_create()
     recognizer.read('train/trainner.yml')
-    faces,gray = getFaces(path)
-    
+    cascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath);
+    color = Image.open(path)
+    color = np.array(color)
+    img = Image.open(path).convert('L')
+    gray = np.array(img,'uint8')
+    faces=faceCascade.detectMultiScale(gray,1.2,5)
     if len(faces) > 0 :
         for(x,y,w,h) in faces:
+            cv2.rectangle(color,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
             Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
-        if(conf<48):
+
+        if(conf<50):
+
             return (Id,conf,"",True)
         else:
-            return (Id,conf,"problem in recognising face please retrain the model",False)
+
+            return (0,0,"problem in recognising face please retrain the model",False)
     else:
+
         return(0,0,"No face Found",False)
 
 
@@ -163,24 +168,3 @@ def nukedir(dir):
             os.unlink(path)
     os.rmdir(dir)
 
-
-def getFaces(path):
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    req_size = 800,800
-    _tempImg = Image.open(path)
-    _tempImgSize = _tempImg.size
-    x,y = _tempImgSize
-    if _tempImgSize > req_size :
-        _tempImg.thumbnail(req_size, Image.ANTIALIAS)
-        _tempImg.save(path,optimize=True)
-        if x > y :
-            img = Image.open(path)
-            temp = img.rotate(-90,expand = True)
-            temp.save(path)
-    img = cv2.imread(path)
-    minisize = (img.shape[1],img.shape[0])
-    miniframe = cv2.resize(img, minisize)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(miniframe,1.3,5)
-    return faces,gray
